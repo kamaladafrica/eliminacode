14ce2d5c6a3b873abab6b68c3ae854e7396222de
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2020-04-18 15:54:03 +0200

    fix app config

--- src/main/java/it/kamaladafrica/eliminacode/config/ApplicationProperties.java
+++ src/main/java/it/kamaladafrica/eliminacode/config/ApplicationProperties.java
@@ -10,4 +10,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+	private int qrcodeSize = 160;
+	private double averageTempo = 2.5; // minuti
+
+	public double getAverageTempo() {
+		return averageTempo;
+	}
+
+	public void setAverageTempo(double averageTempo) {
+		this.averageTempo = averageTempo;
+	}
+
+	public int getQrcodeSize() {
+		return qrcodeSize;
+	}
+
+	public void setQrcodeSize(int qrcodeSize) {
+		this.qrcodeSize = qrcodeSize;
+	}
+
 }
