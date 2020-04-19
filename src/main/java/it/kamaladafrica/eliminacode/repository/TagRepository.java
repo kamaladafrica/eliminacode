0378a6f29abe7bddb33de377493d8f02d9a853b2
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2020-04-19 05:10:54 +0200

    repository

--- src/main/java/it/kamaladafrica/eliminacode/repository/TagRepository.java
+++ src/main/java/it/kamaladafrica/eliminacode/repository/TagRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import it.kamaladafrica.eliminacode.domain.Tag;
@@ -19,14 +19,19 @@ import it.kamaladafrica.eliminacode.domain.Tag;
 @SuppressWarnings("unused")
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long>, JpaSpecificationExecutor<Tag> {
+
 	Optional<Tag> findByKey(UUID key);
 
-	Page<Tag> findAllByStaccatoGreaterThanEqualAndBruciatoIsNotNull(Instant staccato, Pageable page);
+	@Query("select t.progressivo from Tag t where t.staccato >= CURRENT_DATE and t.progressivo > nvl((select max(x.progressivo) from Tag x where x.bruciato = (select max(y.bruciato) from Tag y)), 0) order by t.progressivo asc")
+	List<Long> findNextProgressivi();
 
-	Page<Tag> findAllByStaccatoGreaterThanEqualAndBruciatoIsNull(Instant staccato, Pageable page);
+	@Query("select max(t.bruciato) from Tag t where t.staccato >= CURRENT_DATE")
+	Optional<Instant> getLastBruciatoInstantOfToday();
 
-	List<Tag> findTop1000ByBruciatoIsNotNullOrderByBruciatoDesc();
+	@Query("select t from Tag t where t.staccato >= CURRENT_DATE and t.progressivo = (select max(x.progressivo) from Tag x where x.bruciato = (select max(y.bruciato) from Tag y))")
+	Optional<Tag> getLastBruciatoOfToday();
 
-	List<Tag> findAllByStaccatoGreaterThanEqualOrderByProgressivoDesc(Instant staccato);
+	@Query("select t from Tag t where key = :key and t.staccato >= CURRENT_DATE and t.bruciato is null")
+	Optional<Tag> findByKeyNotBruciatoOfToday(@Param("key") UUID key);
 
 }
